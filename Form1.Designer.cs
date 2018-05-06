a34c19483a57fb4ffb93b996d7ff1829c4ac2eea
Sangmin <38128568+SangminKW@users.noreply.github.com>
2018-05-06 19:43:19 +0900

    UI 구현 및 몇 가지 아이콘 클릭 활성화 (#1)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,9 +31,23 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGoraniBrowser));
             this.wbMain = new System.Windows.Forms.WebBrowser();
             this.txtUrl = new System.Windows.Forms.TextBox();
-            this.btnBefore = new System.Windows.Forms.Button();
-            this.btnForward = new System.Windows.Forms.Button();
-            this.btnGo = new System.Windows.Forms.Button();
+            this.pnlBookmark = new System.Windows.Forms.Panel();
+            this.picNewTab = new System.Windows.Forms.PictureBox();
+            this.picBookmark = new System.Windows.Forms.PictureBox();
+            this.picMenu = new System.Windows.Forms.PictureBox();
+            this.picMemo = new System.Windows.Forms.PictureBox();
+            this.picHome = new System.Windows.Forms.PictureBox();
+            this.picRefresh = new System.Windows.Forms.PictureBox();
+            this.picForward = new System.Windows.Forms.PictureBox();
+            this.picBack = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picNewTab)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBookmark)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMenu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMemo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHome)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRefresh)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picForward)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack)).BeginInit();
             this.SuspendLayout();
             // 
             // wbMain
@@ -41,12 +55,12 @@
             this.wbMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.wbMain.Location = new System.Drawing.Point(14, 49);
+            this.wbMain.Location = new System.Drawing.Point(14, 100);
             this.wbMain.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.wbMain.MinimumSize = new System.Drawing.Size(23, 25);
             this.wbMain.Name = "wbMain";
             this.wbMain.ScriptErrorsSuppressed = true;
-            this.wbMain.Size = new System.Drawing.Size(923, 534);
+            this.wbMain.Size = new System.Drawing.Size(923, 554);
             this.wbMain.TabIndex = 4;
             this.wbMain.Url = new System.Uri("http://www.google.com/", System.UriKind.Absolute);
             this.wbMain.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.wbMain_DocumentCompleted);
@@ -55,66 +69,144 @@
             // 
             this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtUrl.Location = new System.Drawing.Point(112, 15);
+            this.txtUrl.Location = new System.Drawing.Point(178, 63);
             this.txtUrl.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtUrl.Name = "txtUrl";
-            this.txtUrl.Size = new System.Drawing.Size(735, 25);
+            this.txtUrl.Size = new System.Drawing.Size(636, 25);
             this.txtUrl.TabIndex = 0;
             this.txtUrl.Text = "http://www.google.com/";
             this.txtUrl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUrl_KeyDown);
             // 
-            // btnBefore
+            // pnlBookmark
             // 
-            this.btnBefore.BackColor = System.Drawing.Color.Black;
-            this.btnBefore.Location = new System.Drawing.Point(14, 15);
-            this.btnBefore.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnBefore.Name = "btnBefore";
-            this.btnBefore.Size = new System.Drawing.Size(43, 26);
-            this.btnBefore.TabIndex = 2;
-            this.btnBefore.Text = "<";
-            this.btnBefore.UseVisualStyleBackColor = true;
-            this.btnBefore.Click += new System.EventHandler(this.btnBefore_Click);
+            this.pnlBookmark.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlBookmark.BackColor = System.Drawing.Color.Transparent;
+            this.pnlBookmark.Location = new System.Drawing.Point(12, 100);
+            this.pnlBookmark.Name = "pnlBookmark";
+            this.pnlBookmark.Size = new System.Drawing.Size(927, 554);
+            this.pnlBookmark.TabIndex = 12;
+            this.pnlBookmark.Visible = false;
             // 
-            // btnForward
+            // picNewTab
             // 
-            this.btnForward.BackColor = System.Drawing.Color.Black;
-            this.btnForward.Location = new System.Drawing.Point(64, 15);
-            this.btnForward.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnForward.Name = "btnForward";
-            this.btnForward.Size = new System.Drawing.Size(43, 26);
-            this.btnForward.TabIndex = 3;
-            this.btnForward.Text = ">";
-            this.btnForward.UseVisualStyleBackColor = true;
-            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            this.picNewTab.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.picNewTab.Image = global::GoraniBrowser.Properties.Resources.NewTab;
+            this.picNewTab.Location = new System.Drawing.Point(913, 15);
+            this.picNewTab.Name = "picNewTab";
+            this.picNewTab.Size = new System.Drawing.Size(25, 25);
+            this.picNewTab.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picNewTab.TabIndex = 13;
+            this.picNewTab.TabStop = false;
             // 
-            // btnGo
+            // picBookmark
             // 
-            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnGo.BackColor = System.Drawing.Color.Black;
-            this.btnGo.Location = new System.Drawing.Point(855, 15);
-            this.btnGo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnGo.Name = "btnGo";
-            this.btnGo.Size = new System.Drawing.Size(82, 26);
-            this.btnGo.TabIndex = 1;
-            this.btnGo.Text = "GO";
-            this.btnGo.UseVisualStyleBackColor = true;
-            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            this.picBookmark.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBookmark.Image = global::GoraniBrowser.Properties.Resources.Bookmark;
+            this.picBookmark.Location = new System.Drawing.Point(820, 53);
+            this.picBookmark.Name = "picBookmark";
+            this.picBookmark.Size = new System.Drawing.Size(35, 35);
+            this.picBookmark.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBookmark.TabIndex = 11;
+            this.picBookmark.TabStop = false;
+            this.picBookmark.Click += new System.EventHandler(this.picBookmark_Click);
+            // 
+            // picMenu
+            // 
+            this.picMenu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.picMenu.Image = global::GoraniBrowser.Properties.Resources.Menu;
+            this.picMenu.Location = new System.Drawing.Point(902, 53);
+            this.picMenu.Name = "picMenu";
+            this.picMenu.Size = new System.Drawing.Size(35, 35);
+            this.picMenu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMenu.TabIndex = 10;
+            this.picMenu.TabStop = false;
+            // 
+            // picMemo
+            // 
+            this.picMemo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.picMemo.Image = global::GoraniBrowser.Properties.Resources.Memo;
+            this.picMemo.Location = new System.Drawing.Point(861, 53);
+            this.picMemo.Name = "picMemo";
+            this.picMemo.Size = new System.Drawing.Size(35, 35);
+            this.picMemo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMemo.TabIndex = 9;
+            this.picMemo.TabStop = false;
+            // 
+            // picHome
+            // 
+            this.picHome.Image = global::GoraniBrowser.Properties.Resources.Home;
+            this.picHome.Location = new System.Drawing.Point(137, 53);
+            this.picHome.Name = "picHome";
+            this.picHome.Size = new System.Drawing.Size(35, 35);
+            this.picHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picHome.TabIndex = 8;
+            this.picHome.TabStop = false;
+            this.picHome.Click += new System.EventHandler(this.picHome_Click);
+            // 
+            // picRefresh
+            // 
+            this.picRefresh.Image = global::GoraniBrowser.Properties.Resources.Refresh;
+            this.picRefresh.Location = new System.Drawing.Point(96, 53);
+            this.picRefresh.Name = "picRefresh";
+            this.picRefresh.Size = new System.Drawing.Size(35, 35);
+            this.picRefresh.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picRefresh.TabIndex = 7;
+            this.picRefresh.TabStop = false;
+            this.picRefresh.Click += new System.EventHandler(this.picRefresh_Click);
+            // 
+            // picForward
+            // 
+            this.picForward.Image = global::GoraniBrowser.Properties.Resources.Forword;
+            this.picForward.Location = new System.Drawing.Point(55, 53);
+            this.picForward.Name = "picForward";
+            this.picForward.Size = new System.Drawing.Size(35, 35);
+            this.picForward.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picForward.TabIndex = 6;
+            this.picForward.TabStop = false;
+            this.picForward.Click += new System.EventHandler(this.picForward_Click);
+            // 
+            // picBack
+            // 
+            this.picBack.Image = ((System.Drawing.Image)(resources.GetObject("picBack.Image")));
+            this.picBack.Location = new System.Drawing.Point(14, 53);
+            this.picBack.Name = "picBack";
+            this.picBack.Size = new System.Drawing.Size(35, 35);
+            this.picBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBack.TabIndex = 5;
+            this.picBack.TabStop = false;
+            this.picBack.Click += new System.EventHandler(this.picBack_Click);
             // 
             // frmGoraniBrowser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(951, 598);
-            this.Controls.Add(this.btnGo);
-            this.Controls.Add(this.btnForward);
-            this.Controls.Add(this.btnBefore);
+            this.ClientSize = new System.Drawing.Size(951, 669);
+            this.Controls.Add(this.picNewTab);
+            this.Controls.Add(this.pnlBookmark);
+            this.Controls.Add(this.picBookmark);
+            this.Controls.Add(this.picMenu);
+            this.Controls.Add(this.picMemo);
+            this.Controls.Add(this.picHome);
+            this.Controls.Add(this.picRefresh);
+            this.Controls.Add(this.picForward);
+            this.Controls.Add(this.picBack);
             this.Controls.Add(this.txtUrl);
             this.Controls.Add(this.wbMain);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmGoraniBrowser";
             this.Text = "Gorani Browser";
+            ((System.ComponentModel.ISupportInitialize)(this.picNewTab)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBookmark)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMenu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMemo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHome)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRefresh)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picForward)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,9 +216,15 @@
 
         private System.Windows.Forms.WebBrowser wbMain;
         private System.Windows.Forms.TextBox txtUrl;
-        private System.Windows.Forms.Button btnBefore;
-        private System.Windows.Forms.Button btnForward;
-        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.PictureBox picBack;
+        private System.Windows.Forms.PictureBox picForward;
+        private System.Windows.Forms.PictureBox picRefresh;
+        private System.Windows.Forms.PictureBox picHome;
+        private System.Windows.Forms.PictureBox picMemo;
+        private System.Windows.Forms.PictureBox picMenu;
+        private System.Windows.Forms.PictureBox picBookmark;
+        private System.Windows.Forms.Panel pnlBookmark;
+        private System.Windows.Forms.PictureBox picNewTab;
     }
 }
 
